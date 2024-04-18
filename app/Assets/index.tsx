4ec79f17174493cdeb26a862e5f8204d56143bfb
Priya Biswas <priyabiswas5597@gmail.com>
2024-04-18 22:30:13 +0600

    image add

---
+++ app/Assets/index.tsx
@@ -0,0 +1,9 @@
+'use strict';
+
+const Assets = {
+  splash: require('./Images/splash.png'),
+  img_hideeye:require('../Assets/Images/img_hideeye.png'),
+  Eye: require('./Images/Eye1.png'),
+};
+
+export default Assets;
