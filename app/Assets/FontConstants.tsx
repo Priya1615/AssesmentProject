27d1baa34356af18362f5b93bd4d8d4a9e293bc5
Priya Biswas <priyabiswas5597@gmail.com>
2024-04-18 22:29:11 +0600

    font add

---
+++ app/Assets/FontConstants.tsx
@@ -0,0 +1,9 @@
+export const FontConstants = {
+    Avenir400:"Avenir Next Font",
+    Avenir600:"avenir-next-demibold",
+    Avenir700:"avenir-next-bold",
+    Avenir500:"avenir-next-medium",
+    AvenirItalic:"Avenir Next LT Pro Italic"
+};
+
+
