a0a1880d5a93c0068c7ecddae410217c7e47320b
Priya Biswas <priyabiswas5597@gmail.com>
2024-04-19 22:20:59 +0600

    image add

--- app/Assets/index.tsx
+++ app/Assets/index.tsx
@@ -4,6 +4,7 @@ const Assets = {
   splash: require('./Images/splash.png'),
   img_hideeye:require('../Assets/Images/img_hideeye.png'),
   Eye: require('./Images/Eye1.png'),
+  delete:require('./Images/delete_gray.png'),
 };
 
 export default Assets;
