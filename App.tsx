6493a3e0c02e0e53bc418e83843f5e784ab99b6d
Priya Biswas <priyabiswas5597@gmail.com>
2024-04-21 00:33:01 +0600

    firebase fix

--- App.tsx
+++ App.tsx
@@ -2,14 +2,11 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import StackRoutes from './app/Navigation/Routes';
 import {firebase} from '@react-native-firebase/auth';
-import {getApp} from '@react-native-firebase/app';
-import firestore from '@react-native-firebase/firestore';
-import auth from '@react-native-firebase/auth';
 import {firebaseConfig} from './app/Utils/FirebaseHelper';
 
 function App() {
   React.useEffect(() => {
-    if (!getApp()) {
+    if (!firebase.apps.length) {
       firebase.initializeApp(firebaseConfig);
     }
     //
