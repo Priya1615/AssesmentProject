186a880e223a5853bf1f58d6a832734294266cd2
Priya Biswas <priyabiswas5597@gmail.com>
2024-04-21 00:02:36 +0600

    image add

--- app/Assets/index.tsx
+++ app/Assets/index.tsx
@@ -6,6 +6,8 @@ const Assets = {
   Eye: require('./Images/Eye1.png'),
   delete:require('./Images/delete_gray.png'),
   cross:require('./Images/cross.png'),
+  UserProfile:require('./Images/UserProfile.png'),
+  
 };
 
 export default Assets;
