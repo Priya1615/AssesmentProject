8adf6836ec8d3bec29db7ad9448c6aea8c8519a2
Priya Biswas <priyabiswas5597@gmail.com>
2024-04-20 22:17:02 +0600

    task functionality added

--- app/Utils/FirebaseHelper.ts
+++ app/Utils/FirebaseHelper.ts
@@ -38,4 +38,70 @@ export const handleLogin = async (email: string, password: string) => {
     return {err: err};
   }
 };
-
+export const addTodo = async (
+  uid: string,
+  todoText: string,
+  description: string,
+  status: string,
+) => {
+  if (todoText.trim() !== '') {
+    try {
+      await firestore()
+        .collection('todos')
+        .doc(uid)
+        .collection('items')
+        .add({title: todoText, description: description, stat: status})
+        .then(response => {
+          return;
+        });
+    } catch (error) {
+      console.error('Error adding todo: ', error);
+    }
+  }
+};
+export const loadTodos = async (uid: string) => {
+  if (uid) {
+    try {
+      const querySnapshot = await firestore()
+        .collection('todos')
+        .doc(uid)
+        .collection('items')
+        .get();
+      const todosList: any = [];
+      querySnapshot.forEach(doc => {
+        todosList.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      console.log('todo', todosList);
+      return todosList;
+    } catch (error) {
+      console.error('Error loading todos: ', error);
+    }
+  }
+};
+export const updateTodo = async (uid: string, id: string, stat: string) => {
+  try {
+    await firestore()
+      .collection('todos')
+      .doc(uid)
+      .collection('items')
+      .doc(id)
+      .update({stat: stat});
+  } catch (error) {
+    console.error('Error updating todo: ', error);
+  }
+};
+export const deleteTodo = async (uid: string, id: string) => {
+  try {
+    await firestore()
+      .collection('todos')
+      .doc(uid)
+      .collection('items')
+      .doc(id)
+      .delete();
+  } catch (error) {
+    console.error('Error deleting todo: ', error);
+  }
+};
